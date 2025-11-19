c28f46f524fe5675fc3514353f235d91d6465d5c
peter2500zz <2797880090@qq.com>
2025-11-19 17:46:29 +0800

    test: 测试从exe获取icon

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,13 @@
+use std::mem;
+
+use image::RgbaImage;
 use systemicons::get_icon;
 use widestring::U16CString;
 use windows::{
     Win32::{
-        Foundation::{GetLastError, HWND}, 
-        UI::{
+        Foundation::{GetLastError, HWND}, Graphics::Gdi::{BI_RGB, BITMAP, BITMAPINFO, BITMAPINFOHEADER, DIB_RGB_COLORS, DeleteObject, GetBitmapBits, GetDC, GetDIBits, GetObjectW, HBITMAP, HDC, HGDIOBJ, ReleaseDC}, UI::{
             Shell::{
+                ExtractIconExW,
                 SEE_MASK_FLAG_LOG_USAGE, 
                 SEE_MASK_FLAG_NO_UI, 
                 SEE_MASK_INVOKEIDLIST, 
@@ -13,10 +16,7 @@ use windows::{
                 ShellExecuteExW
             }, 
             WindowsAndMessaging::{
-                MB_ICONERROR, 
-                MB_OK, 
-                MessageBoxW, 
-                SW_SHOWNORMAL
+                GetIconInfoExW, HICON, ICONINFO, ICONINFOEXW, MB_ICONERROR, MB_OK, MessageBoxW, SW_SHOWNORMAL
             }
         }
     },
@@ -24,6 +24,133 @@ use windows::{
         PCWSTR, w
     },
 };
+
+
+#[test]
+fn g() {
+    unsafe {
+
+        let hmax = get_hicon_from_exe(r"D:\Files\Rust\hexphage\target\hexphage-0.1.0-windows-msvc-x64.exe")[0];
+
+        println!("{:?}", hmax);
+
+        let mut info = ICONINFOEXW::default();
+        info.cbSize = size_of_val(&info) as _;
+        let _ = GetIconInfoExW(hmax, &mut info);
+        let mut bm = BITMAP::default();
+        GetObjectW(HGDIOBJ(info.hbmColor.0), size_of_val(&bm) as i32, Some(&raw mut bm as _));
+
+        let rgba = convert_icon_to_image(hmax);
+
+        std::fs::create_dir_all(format!("{}/cache/exe_icon", crate::CONFIG_SAVE_PATH)).unwrap();
+
+        let icon_path = format!("{}/cache/exe_icon/gggg.png", crate::CONFIG_SAVE_PATH);
+        rgba.save(icon_path);
+        // if !std::path::Path::new(&icon_path).exists() {
+        //     std::fs::write(icon_path.clone(), rgba.as_ref()).unwrap();
+        // }
+    }
+}
+
+
+fn get_hicon_from_exe(path: &str) -> Vec<HICON> {
+    let u16path = U16CString::from_str(path).unwrap();
+
+    unsafe {
+        let count = ExtractIconExW(
+            PCWSTR(u16path.as_ptr()), 
+            -1,
+            None,
+            None, 
+            0
+        );
+
+        let mut hicons: Vec<HICON> = Vec::new();
+
+        for index in 0..count {
+            let mut hicon = HICON::default();
+            let getted = ExtractIconExW(
+                PCWSTR(u16path.as_ptr()), 
+                index as i32,
+                Some(&mut hicon),
+                None, 
+                1
+            );
+            println!("get {}", getted);
+            if getted > 0 {
+                hicons.push(hicon);
+            }
+        }
+
+        hicons
+    }
+}
+
+/// By [liaohp](https://users.rust-lang.org/u/liaohp/summary)
+/// 
+/// https://users.rust-lang.org/t/how-to-convert-hicon-to-png/90975/15
+/// 
+/// Modified
+unsafe fn convert_icon_to_image(icon: HICON) -> RgbaImage {
+    // 1. 取 ICONINFO
+    let mut info = ICONINFOEXW::default();
+    info.cbSize = size_of_val(&info) as _;
+    let ok = GetIconInfoExW(icon, &mut info);
+    assert!(ok.as_bool()); // 或者自己处理错误
+
+    let mut bitmap = BITMAP::default();
+    let result = GetObjectW(
+        HGDIOBJ(info.hbmColor.0),
+        size_of::<BITMAP>() as i32,
+        Some(&raw mut bitmap as _),
+    );
+    assert!(result == size_of::<BITMAP>() as i32);
+
+    let width = bitmap.bmWidth;
+    let height = bitmap.bmHeight;
+
+    let buf_size = (width * height * 4) as usize;
+    let mut pixels = vec![0u8; buf_size];
+
+    let dc = GetDC(None);
+    assert!(dc != HDC::default());
+
+    let mut bmi = BITMAPINFO {
+        bmiHeader: BITMAPINFOHEADER {
+            biSize: size_of::<BITMAPINFOHEADER>() as _,
+            biWidth: width,
+            biHeight: -height.abs(), // top-down
+            biPlanes: 1,
+            biBitCount: 32,
+            biCompression: BI_RGB.0,
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+
+    let scanlines = GetDIBits(
+        dc,
+        info.hbmColor,
+        0,
+        height as u32,
+        Some(pixels.as_mut_ptr() as _),
+        &mut bmi,
+        DIB_RGB_COLORS,
+    );
+    assert!(scanlines != 0);
+
+    ReleaseDC(None, dc);
+    DeleteObject(HGDIOBJ(info.hbmColor.0));
+    DeleteObject(HGDIOBJ(info.hbmMask.0));
+
+    // BGRA -> RGBA
+    for p in pixels.chunks_exact_mut(4) {
+        p.swap(0, 2); // B <-> R
+    }
+
+    RgbaImage::from_vec(width as u32, height as u32, pixels).unwrap()
+}
+
 
 pub fn get_icon_from_exe(exe_path: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     let sizes = [1024, 512, 256, 128, 96, 64, 48, 32, 24, 16];
